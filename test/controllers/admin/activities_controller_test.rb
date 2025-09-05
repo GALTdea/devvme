76fc3ac531256e699131f00d3afbf24411ee584d
Gustavo Valenzuela <gustavovalenzuela1180@gmail.com>
2025-09-04 18:14:11 -0700

    Enhance admin activities tests with user authentication setup
    - Added a setup block in `Admin::ActivitiesControllerTest` to sign in a test admin user, ensuring proper authentication context for the tests.
    - Updated test cases to use RESTful routes for fetching activities, improving consistency with Rails conventions and enhancing test clarity.

--- test/controllers/admin/activities_controller_test.rb
+++ test/controllers/admin/activities_controller_test.rb
@@ -1,13 +1,19 @@
 require "test_helper"
 
 class Admin::ActivitiesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin = users(:test_admin)
+    sign_in @admin
+  end
+
   test "should get index" do
-    get admin_activities_index_url
+    get admin_activities_url
     assert_response :success
   end
 
   test "should get show" do
-    get admin_activities_show_url
+    activity = admin_activities(:test_activity_one)
+    get admin_activity_url(activity)
     assert_response :success
   end
 end
